912dff9c568d7ded2a4a2131851f9573393cee07
nanako4 <anguuan@outlook.com>
2022-02-01 13:32:55 +0800

    fix vital bug

--- src/jsobject2/_jsobject2.py
+++ src/jsobject2/_jsobject2.py
@@ -7,12 +7,14 @@ Value = TypeVar('Value')
 class JsObject(Generic[Key, Value]):
     def __init__(self, init_dict: Optional[Dict[Key, Value]] = None) -> None:
         self._data = init_dict if init_dict else {}
-        self.__setattr__ = self._setattr
 
     def __getattr__(self, name: str) -> Optional[Value]:
         return self._data.get(name)
 
-    def _setattr(self, name: str, value: Value) -> None:
+    def __setattr__(self, name: str, value: Value) -> None:
+        if name == '_data' and '_data' not in self.__dict__:
+            self.__dict__['_data'] = value
+            return
         self._data[name] = value
 
     def __getitem__(self, key: Key) -> Optional[Value]:
@@ -20,3 +22,6 @@ class JsObject(Generic[Key, Value]):
 
     def __setitem__(self, key: Key, value: Value) -> None:
         self._data[key] = value
+
+    def __contains__(self, key: Key) -> bool:
+        return key in self._data
